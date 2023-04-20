fe5bf3a1c85786137ef6b125357d4fc7fe8a0281
Hugo Rischmann <hugo.rischmann@me.com>
2023-04-20 12:55:02 +0200

    ajout exercice deputes

---
+++ lib/deputes.rb
@@ -0,0 +1,21 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+def get_depute_mail
+  page = Nokogiri::HTML(URI.open('https://www.voxpublic.org/spip.php?page=annuaire&cat=deputes&pagnum=900'))
+  uls = page.xpath('//ul')
+  all_together = []
+  
+  uls.each do |ul|
+    name = ul.xpath('.//h2[@class="titre_normal"]').text.strip
+    email = ul.xpath('.//a[contains(@href, "mailto") and contains(@href, "assemblee")]/text()').text.strip
+    unless email == ""
+      hash = { name => email }
+      all_together << hash
+    end
+  end
+  puts all_together
+end
+
+get_depute_mail
